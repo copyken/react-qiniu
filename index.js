916733b62d43d44c108a3829c5504200368a29cd
zpx <kensmail92@163.com>
2018-05-25 14:56:33 +0800

    Update index.js
    React.PropTypes and React.createClass have moved into different packages. Using the related libraries instead.

--- index.js
+++ index.js
@@ -4,28 +4,30 @@
 var React = require('react');
 var ReactDOM = require('react-dom')
 var request = require('superagent-bluebird-promise');
+var PropTypes = require('prop-types');
+var createClass = require('create-react-class');
 
 var isFunction = function (fn) {
  var getType = {};
  return fn && getType.toString.call(fn) === '[object Function]';
 };
 
-var ReactQiniu = React.createClass({
+var ReactQiniu = createClass({
     // based on https://github.com/paramaggarwal/react-dropzone
     propTypes: {
-        onDrop: React.PropTypes.func.isRequired,
-        token: React.PropTypes.string.isRequired,
+        onDrop: PropTypes.func.isRequired,
+        token: PropTypes.string.isRequired,
         // called before upload to set callback to files
-        onUpload: React.PropTypes.func,
-        size: React.PropTypes.number,
-        style: React.PropTypes.object,
-        supportClick: React.PropTypes.bool,
-        accept: React.PropTypes.string,
-        multiple: React.PropTypes.bool,
+        onUpload: PropTypes.func,
+        size: PropTypes.number,
+        style: PropTypes.object,
+        supportClick: PropTypes.bool,
+        accept: PropTypes.string,
+        multiple: PropTypes.bool,
         // Qiniu
-        uploadUrl: React.PropTypes.string,
-        uploadKey: React.PropTypes.string,
-        prefix: React.PropTypes.string
+        uploadUrl: PropTypes.string,
+        uploadKey: PropTypes.string,
+        prefix: PropTypes.string
     },
 
     getDefaultProps: function() {
